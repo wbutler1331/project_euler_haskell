a13f3ce85ef896e5d82e07d05b6fca00c40969da
wbutler <wbutler1331@gmail.com>
2015-04-09 21:11:31 -0400

    finished 51

--- 51.hs
+++ 51.hs
@@ -1,6 +1,19 @@
-import Data.List
+import Factory.Math.Implementations.Primes.SieveOfEratosthenes
 
-replacementIndexes n = [(start,end) | start <- [0..lenn], end <- [start..lenn]]
+replaceNth n newVal (x:xs)
+     | n == 0 = newVal:xs
+     | otherwise = x:replaceNth (n-1) newVal xs
+
+replacementIndexes n = [(start,end) | start <- [0..lenn], end <- [start+1..lenn]]
 	where
 		lenn = length strn
 		strn = show n
+
+primes = sieveOfEratosthenes 10
+
+main = print
+	$ takeWhile predicate primes
+	where
+		predicate prime =
+			$ map (\n -> replaceNth n newVal (show prime))
+			$ [[start..end] | (start,end) <- replacementIndexes prime, ]
